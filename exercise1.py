cfcf3295e46103b49586eb16934a589af1145f71
Sauran69 <92358678+Sauran69@users.noreply.github.com>
2021-11-05 14:42:16 +0500

    Update exercise1.py

--- exercise1.py
+++ exercise1.py
@@ -8,10 +8,10 @@ lines=sako.readlines()
 Sauran=open ('output.txt','w')
 
 for line in lines:
-   if '(GMT)' in line:
-      ind=line.find(',')
-      en_ind=line.find('+')
-      word=line[ind+2:en_ind-10]
+   if 'Date:' in line:
+      ind=line.find(':')
+      en_ind=line.find('-')
+      word=line[ind+7:en_ind-10]
       print(word)
       Sauran.write(word)
       Sauran.write('\n')
